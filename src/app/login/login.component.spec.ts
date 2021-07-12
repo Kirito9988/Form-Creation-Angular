1b18add6fd26673c52b7ecfd99aaf6d240f6aeb6
[Tarun] <[tarun.1999thakur33@gmail.com]>
2021-07-12 12:27:26 +0530

    first

---
+++ src/app/login/login.component.spec.ts
@@ -0,0 +1,27 @@
+import { AuthService } from '../auth.service';
+
+import { LoginComponent } from './login.component';
+
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let service : AuthService;
+
+  beforeEach(() => { (1)
+    service = new AuthService();
+    component = new LoginComponent(service);
+  });
+
+  afterEach(() => { (2)
+    localStorage.removeItem('token');
+  });
+
+
+  it('needsLogin returns true when the user has not been authenticated', () => {
+    expect(component.needsLogin()).toBeTruthy();
+  });
+
+  it('needsLogin returns false when the user has been authenticated', () => {
+    localStorage.setItem('token', '12345'); (3)
+    expect(component.needsLogin()).toBeFalsy();
+  });
+});
