28a5c29766f9d0336e3d14ca12c03275e6065cb0
[Tarun] <[tarun.1999thakur33@gmail.com]>
2021-07-13 17:09:38 +0530

    [hello new code]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Onproject';
+export class AppComponent implements OnInit {
+ title = "angular test";
+  postDetails: any;
+  showLoadingIndicator: any;
+  text!:'contact page';
+  contactForm!: FormGroup ;
+  contact = {
+    name:'',
+    email:'',
+    text:''
+  };
+submitted = false;
+ 
+constructor(private service : AppService){
+    this.postDetails = [];
+    this.createForm();
+  }
+
+  ngOnInit(){
+    this.getPostDetails();
+  }
+
+  onSubmit(){
+    this.submitted = true;
+  }
+
+  getPostDetails(){
+    this.showLoadingIndicator = true;
+    this.service.getPosts().subscribe((response:any) => {
+      this.showLoadingIndicator = false;
+      if(response && response.length > 0) {
+        this.postDetails = response;
+      } else {
+        this.postDetails = [];
+      }
+    })
+  }
+  createForm(){
+    this.contactForm = new FormGroup({
+      'name' : new FormControl(this.contact.name,[
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'email' : new FormControl(this.contact.email,[
+        Validators.required,
+        Validators.email
+      ]),
+      'text' : new FormControl(this.contact.name,[
+        Validators.required,
+      ]),
+    })
+  }
 }
